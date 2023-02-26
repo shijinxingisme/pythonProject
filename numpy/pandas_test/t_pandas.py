c86abcab6331277534eebc74d3d2fcae34ae4d94
sjx <shijinxingisme@gmail.com>
2023-02-26 23:28:11 +0800

    fix

--- numpy/pandas_test/t_pandas.py
+++ numpy/pandas_test/t_pandas.py
@@ -34,6 +34,30 @@ print(s3)
 
 data = {'year': [2014, 2015, 2016],
         'income': [1000, 2000, 3000],
-        'pay': [500, 1000, 1500]}
+        'pay': [500, 1000, 2500]}
 df1 = pd.DataFrame(data)
 print(df1)
+
+df2 = pd.DataFrame(np.arange(12).reshape((3,4)))        #3行4列 传入dataframe
+print(df2)
+df3 = pd.DataFrame(np.arange(12).reshape((3,4)),index=["z","s","t"],columns=['a','c','b','d'])
+print(df3)
+
+print("df1 columns:",df1.columns)
+print("df2 index:",df2.index)
+print("df3 values:",df3.values)
+# count 数量  mean平均值 std标准差  min最小值  max最大值
+print("describe:",df1.describe())       #查看
+print("describe T: \n",df1.T)       #转置
+print(df3)
+print(df3.sort_index(axis=1))   #列索引排序    columns
+print(df3.sort_index(axis=0))   #行索引排序    index
+
+print(df3.sort_values(by='b'))   # 列值排序   columns
+print(df3.sort_values(by='d'))   # 列值排序   columns
+
+#describe 分析 https://zhuanlan.zhihu.com/p/56526297
+
+# https://blog.csdn.net/mahoon411/article/details/114777623  axis参数详解
+
+
---
+++ numpy/pandas_test/t_pandas2.py
@@ -0,0 +1,4 @@
+'''
+pandas数据选择
+https://www.bilibili.com/video/BV1xa4y1e7az/?p=2&spm_id_from=pageDriver&vd_source=d45e7fa6827868e5c143d3c05fd5e52e
+'''
